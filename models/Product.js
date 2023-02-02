bca49fe53d240a105dd6af86615a7c1094c6399a
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-02-01 23:42:19 -0600

    Added column data in product.js

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,6 +9,23 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: String,
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull: true
+    },
+    Stock: INT,
+    category_id: INT,
+    category: {
+      id:INT,
+      category_name: STRING
+    }
     // define columns
   },
   {
