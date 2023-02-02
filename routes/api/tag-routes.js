b71552599e910fb20d3b90930aface597407bf1e
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-02-02 00:39:45 -0600

    Added post ro tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,15 +41,18 @@ router.get('/:id', async (req, res) => {
 // create a new tag
 router.post('/', async (req, res) => {
   try {
-    
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
   
 });
 
+
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+ 
 });
 
 
