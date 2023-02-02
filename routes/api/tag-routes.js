c3895067e916a7e3888a2aa290f86be04f9de41e
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-02-02 00:35:48 -0600

    Added delete router in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,16 +38,40 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', async (req, res) => {
+  try {
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+
+ // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json ({ message: 'No tag found with that id!'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
