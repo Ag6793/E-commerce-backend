fd095a1f439b7c929a93e7be8da48b6f432f7e4c
Alexandria Guerrero <107378358+Ag6793@users.noreply.github.com>
2023-02-02 00:15:46 -0600

    Renamed foreign keys

--- models/index.js
+++ models/index.js
@@ -6,31 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
   through: {
     model: ProductTag,
-    unique: false
   },
   as: 'product_tags'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
   through: {
     model:ProductTag,
-    unique:false
   },
-
-  as: "tags"
 })
 
 module.exports = {
